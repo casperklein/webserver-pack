6b3da2b2ab3b78b4e5303b430b311a5f2a6af488
casperklein <casperklein@users.noreply.github.com>
2019-09-03 00:59:08 +0200

    modified:   docker-run.sh

--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -8,9 +8,3 @@ patch /etc/apache2/sites-available/$DOMAIN -i docker/docker-ssl.patch
 apachectl restart
 
 find /var/domains/ -name '*.log' -not -name '*gz' -print0 | xargs -0 tail -F
-
-exit
-while :; do
-	sleep 1
-	echo -n .
-done
