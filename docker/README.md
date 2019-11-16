043d316ea37c804966719cfe472d230b2fb805ba
casperklein <casperklein@users.noreply.github.com>
2019-11-17 00:14:45 +0100

    README.md added

---
+++ docker/README.md
@@ -0,0 +1,19 @@
+# Let's encrypt POC
+
+POC to show the setup of Apache webserver with a *Let's encrypt* TLS certificate.
+
+## Prerequisites
+
+Make sure, that your host ports tcp/80 & tcp/443 are exposed to the internet.
+
+## Build
+
+    make build
+
+## Start POC
+
+    make poc
+
+## Cleanup build environment
+
+    make clean
--- docker/docker-run.sh
+++ docker/docker-run.sh
@@ -7,4 +7,11 @@ patch /etc/apache2/sites-available/$DOMAIN -i docker/docker-ssl.patch
 
 apachectl restart
 
+echo "###############################################################"
+echo "Apache is now serving:"
+echo
+echo "http://$DOMAIN"
+echo "https://$DOMAIN"
+echo "###############################################################"
+
 find /var/domains/ -name '*.log' -not -name '*gz' -print0 | xargs -0 tail -F
